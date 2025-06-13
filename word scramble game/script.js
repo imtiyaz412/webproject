aa553747141e16874ef39d2c69018b477559cd4d
imtiyaz412 <imtiyazhussain018@gmail.com>
2025-06-14 01:36:43 +0530

    Add files via upload

---
+++ word scramble game/script.js
@@ -0,0 +1,37 @@
+let words=[ 
+{
+    word: "addition",
+    hint:"the process of adding number"
+},
+{
+   word: "meeting",
+    hint:"event in which people come together"
+},
+{
+     word: "number",
+    hint:"math symbol used for counting"
+},
+{
+     word: "exchange",
+    hint:"the act of trading"
+},
+{
+     word: "canvas",
+    hint:"piece of fabric for oil painting"
+},
+{
+     word: "garden",
+    hint:"space for planting flower and plant"
+},
+{
+     word: "position",
+    hint:"location of someone or somethings"
+},
+{
+     word: "feather",
+    hint:"hair like outer covering of bird"
+},
+{
+     word: "comfort",
+    hint:"a plesant feeling of relaxation"
+},
